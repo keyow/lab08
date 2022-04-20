1e0b3a413c7b2c3f902e2dddcb9b5d5039c6940d
keyow <kirill500123@gmail.com>
2022-04-21 02:35:52 +0300

    removed some files

--- cmake-build-debug/CMakeFiles/fib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libfibd.a"
-)
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/keyow/workspace/projects/lab07
-# Build directory: /home/keyow/workspace/projects/lab07/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/keyow/workspace/projects/lab07/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/keyow/workspace/projects/lab07/CMakeLists.txt;35;add_test;/home/keyow/workspace/projects/lab07/CMakeLists.txt;0;")
